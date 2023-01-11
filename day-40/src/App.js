91ae56d810a2b2f0e050335d3749c458580911d7
Your Name <morning@mstars-lab2-01s-iMac.local>
2023-01-11 09:47:45 +0800

    Day-30

---
+++ day-40/src/App.js
@@ -0,0 +1,17 @@
+// import logo from './logo.svg';
+import './App.css';
+import User from './components/User';
+
+function App() {
+  // const user = userList.map((e) =>{
+  //   return <User name={e.name}/>
+  // })
+  return (
+    <div className="App">
+      <h1>Day-40 React State</h1>
+      <User />
+    </div>
+  );
+}
+
+export default App;
---
+++ day-40/src/data/data.js
@@ -0,0 +1,16 @@
+const userList = [
+    {
+        name: 'Suvdmaa',
+    },
+    {
+        name: "Khongoroo",
+    },
+    {
+        name: "Tushig",
+    },
+    {
+        name: "Munkh-Erdene"
+    }
+]
+
+export default userList
