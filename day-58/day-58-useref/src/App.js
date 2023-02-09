4573ca59f331c04a22b389c305c05d288881859b
Your Name <morning@mstars-lab2-01s-iMac.local>
2023-02-09 10:52:05 +0800

    day-59

--- day-58/day-58-useref/src/App.js
+++ day-58/day-58-useref/src/App.js
@@ -1,11 +1,13 @@
 import { useContext } from "react";
 import "./App.css";
-import List from "./components/List";
-import StopWatch from "./components/Stopwatch";
+import InputFocus from "./components/InputFocus";
+import VideoPlayer from "./components/VideoPlayer";
+// import List from "./components/List";
+// import StopWatch from "./components/Stopwatch";
 import { ComponentContext } from "./context/Context";
 
 function App() {
-  const [isLarge, setIsLarge, imageSize] = useContext(ComponentContext);
+  // const [isLarge, setIsLarge, imageSize] = useContext(ComponentContext);
 
   return (
     <div className="App">
@@ -17,7 +19,11 @@ function App() {
       <hr />
       <List /> */}
       <h1>Day - 58 useRef Hook</h1>
-      <StopWatch />
+      {/* <StopWatch /> */}
+      {/* <InputFocus /> */}
+
+      <h1>Video PLayer</h1>
+      <VideoPlayer />
     </div>
   );
 }
