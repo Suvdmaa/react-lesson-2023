d563495f11dbc320ccff8e437fd3e62457654f92
Your Name <morning@mstars-lab2-01s-iMac.local>
2023-02-15 12:35:59 +0800

    day-63

--- day-62-user-crud-admin/BE/index.js
+++ day-62-user-crud-admin/BE/index.js
@@ -3,9 +3,11 @@ console.log("Day - 62 - User Login Crud");
 const express = require("express");
 const cors = require("cors");
 const fs = require("fs");
+const bcrypt = require("bcrypt");
 
 const app = express();
 const PORT = 8080;
+const SALT_ROUNDS = 10;
 
 app.use(cors());
 app.use(express.json());
@@ -27,6 +29,69 @@ app.get("/register", (request, response) => {
   });
 });
 
+// API Login
+app.post("/login", (request, response) => {
+  const body = request.body;
+  console.log(body);
+
+  fs.readFile("./data/users.json", "utf-8", (readError, readData) => {
+    if (readError) {
+      response.json({
+        status: "file not found",
+        data: [],
+      });
+    }
+
+    const usersArrayObject = JSON.parse(readData);
+
+    const foundUser = usersArrayObject.filter(
+      (user) => body.email === user.email
+    );
+
+    if (foundUser.length === 0) {
+      response.json({
+        status: "user not found",
+        data: [],
+      });
+    } else {
+      //hervee hereglegch oldson bol
+      const foundUserObj = foundUser[0];
+      console.log(foundUserObj);
+      const plainPassword = body.password;
+      const savedPaswword = foundUserObj.password;
+
+      bcrypt.compare(
+        plainPassword,
+        savedPaswword,
+        (compareError, compareResult) => {
+          if (compareError) {
+            response.json({
+              status: "Username or password do not match",
+              data: [],
+            });
+          }
+          if (compareResult) {
+            response.json({
+              status: "success",
+              data: {
+                email: foundUserObj.email,
+                firstname: foundUserObj.firstname,
+                lastname: foundUserObj.lastname,
+              },
+            });
+          } else {
+            response.json({
+              status: "Username or Password do not match",
+              data: [],
+            });
+          }
+        }
+      );
+    }
+  });
+});
+
+//API get all user
 app.post("/register", (request, response) => {
   const body = request.body;
   console.log(body);
@@ -54,30 +119,55 @@ app.post("/register", (request, response) => {
       const roleData = JSON.parse(readData);
       const roleName = roleData.filter((role) => role.id === body.role)[0];
 
-      const userData = {
-        ...body,
-        role: roleName,
-      };
+      const userPassword = body.password;
 
-      dataObject.push(userData);
+      bcrypt.genSalt(SALT_ROUNDS, (err, salt) => {
+        if (err) {
+          response.json({
+            status: "generating salt error",
+            data: [],
+          });
+        }
 
-      // fs write
-      fs.writeFile(
-        "./data/users.json",
-        JSON.stringify(dataObject),
-        (writeError) => {
-          if (writeError) {
+        bcrypt.hash(userPassword, salt, (hashError, hashData) => {
+          if (hashError) {
             response.json({
-              status: "Error durrong file write",
+              status: "hashing has error",
               data: [],
             });
           }
-          response.json({
-            status: "success",
-            data: dataObject,
-          });
-        }
-      );
+
+          console.log("hashed Data", hashData);
+          const newUser = {
+            firstname: body.firstname,
+            lastname: body.lastname,
+            email: body.email,
+            password: hashData,
+            address: body.address,
+            role: roleName,
+          };
+
+          dataObject.push(newUser);
+
+          // fs write
+          fs.writeFile(
+            "./data/users.json",
+            JSON.stringify(dataObject),
+            (writeError) => {
+              if (writeError) {
+                response.json({
+                  status: "Error durrong file write",
+                  data: [],
+                });
+              }
+              response.json({
+                status: "success",
+                data: dataObject,
+              });
+            }
+          );
+        });
+      });
     });
   });
 });
