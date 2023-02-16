d59014d1c44ba9674ea22e3d27b1843195df79c8
Your Name <morning@mstars-lab2-01s-iMac.local>
2023-02-16 12:07:06 +0800

    added search, edit , delete

--- day-63-product-category-crud/be/index.js
+++ day-63-product-category-crud/be/index.js
@@ -17,6 +17,7 @@ app
   .post((request, response) => {
     const body = request.body;
     console.log(body);
+    const isEdit = body.isEdit;
 
     const categoryData = fs.readFileSync("./data/categories.json", {
       encoding: "utf-8",
@@ -24,12 +25,24 @@ app
     });
 
     const categoryDataObj = JSON.parse(categoryData);
-    const newCategory = {
-      id: uuid(),
-      name: body.catName,
-    };
 
-    categoryDataObj.push(newCategory);
+    // request ni edit
+    if (isEdit) {
+      categoryDataObj.map((category) => {
+        if (category.id == body.categoryId) {
+          category.name = body.categoryName;
+        }
+        return category;
+      });
+    } else {
+      // request ni new category
+      const newCategory = {
+        id: uuid(),
+        name: body.categoryName,
+      };
+
+      categoryDataObj.push(newCategory);
+    }
 
     const writeCategoryData = fs.writeFileSync(
       "./data/categories.json",
@@ -58,7 +71,73 @@ app
       status: "success",
       data: JSON.parse(readCategoryData),
     });
+  })
+  .delete((request, response) => {
+    const body = request.body;
+    console.log(body);
+
+    const savedCategories = fs.readFileSync("./data/categories.json", {
+      encoding: "utf-8",
+      flag: "r",
+    });
+
+    const savedCategoriesObj = JSON.parse(savedCategories);
+
+    console.log(savedCategoriesObj.legnth);
+    const filteredCategories = savedCategoriesObj.filter(
+      (category) => category.id !== body.categoryId
+    );
+
+    console.log(filteredCategories.length);
+    fs.writeFileSync(
+      "./data/categories.json",
+      JSON.stringify(filteredCategories)
+    );
+
+    response.json({
+      status: "success",
+      data: filteredCategories,
+    });
+  })
+  .put((request, response) => {
+    const body = request.body;
+    console.log(body);
+    const catId = body.categoryId;
+
+    const savedCategories = fs.readFileSync("./data/categories.json", {
+      encoding: "utf-8",
+      flag: "r",
+    });
+
+    const savedCategoriesObjectArray = JSON.parse(savedCategories);
+    const foundCategory = savedCategoriesObjectArray.filter(
+      (category) => category.id == catId
+    )[0];
+
+    response.json({
+      status: "success",
+      data: foundCategory,
+    });
   });
+
+app.get("/search", (request, response) => {
+  console.log(request.query);
+  const savedCategories = fs.readFileSync("./data/categories.json", {
+    encoding: "utf-8",
+    flag: "r",
+  });
+
+  const savedCategoriesArrayObject = JSON.parse(savedCategories);
+
+  const foundCategory = savedCategoriesArrayObject.filter(
+    (category) => category.name == request.query.value
+  );
+
+  response.json({
+    status: "success",
+    data: foundCategory,
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
