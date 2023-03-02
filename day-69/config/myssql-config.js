eb7501c544dbd7b8f636356337a8f7664ed84b61
Your Name <morning@mstars-lab2-01s-iMac.local>
2023-03-02 12:17:37 +0800

    aad day 69 for crud app

---
+++ day-69/config/myssql-config.js
@@ -0,0 +1,9 @@
+import mysql from "mysql2/promise";
+
+export const pool = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  //   post: '3307',
+  password: "",
+  database: "morning",
+});
