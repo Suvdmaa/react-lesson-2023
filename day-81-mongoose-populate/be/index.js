a3418a9d8ff1aa51d79734bad1a0912222706167
Your Name <morning@mstars-lab2-01s-iMac.local>
2023-03-21 11:41:39 +0800

    day-81 INSERT/UPDATE/DELETE Many

---
+++ day-81-mongoose-populate/be/index.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+const Todo = require("./models/Todo");
+
+const app = express();
+const PORT = 8080;
+const MONGO_CONNECTION_STRING =
+  "mongodb+srv://blackenkhboldsuvdmaa:k7zDjv%40K!t%40TsF@suvd.mba0zd4.mongodb.net/test";
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (request, response) => {
+  response.status(200).send("<h1>Day-81 Insert/Update/Delete Many</h1>");
+});
+
+app.get("/list", async (request, response) => {
+  const result = await Todo.find({});
+  response.status(200).json({ data: result });
+});
+
+app.post("/create", async (request, response) => {
+  const body = request.body;
+  const result = await Todo.insertMany(body);
+  response.status(200).json({ data: result });
+});
+
+app.put("/update", async (request, response) => {
+  const body = request.body;
+  const result = await Todo.updateMany(
+    { name: body[1].name },
+    { $set: { checked: body[0].checked } }
+  );
+  response.status(200).json({ data: result });
+});
+
+app.delete("/delete", async (request, response) => {
+  const body = request.body;
+  const result = await Todo.deleteMany(body);
+  response.status(200).json({ data: result });
+});
+
+app.listen(PORT, () => {
+  mongoose
+    .connect(MONGO_CONNECTION_STRING)
+    .then(() => console.log("Database connected succesfully"))
+    .catch((error) => console.error(error));
+  console.log(`Application running on http://localhost:${PORT}`);
+});
