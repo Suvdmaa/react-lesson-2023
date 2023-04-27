d13c734339a8abf243c2ea21ff8b028194a9aba2
Suvdmaa <e.suvd1230@gmail.com>
2023-04-27 11:40:07 +0800

    did sign in and out also did oauth facebook

--- day-102-oauth-with-nextauth/src/components/header.tsx
+++ day-102-oauth-with-nextauth/src/components/header.tsx
@@ -1,23 +1,62 @@
-import { signIn, useSession } from 'next-auth/react'
+import { url } from 'inspector'
+import { signOut, signIn, useSession } from 'next-auth/react'
+import styles from '../styles/header.module.css'
 
 export default function Header() {
   const { data: session, status } = useSession()
+  const loading = status === 'loading'
 
   const handleLogin = (e: any) => {
     e.preventDefault()
     signIn()
   }
 
+  const handleLogout = (e: any) => {
+    e.preventDefault()
+    signOut()
+  }
+
+  console.log(session)
   return (
     <header>
-      <div>
-        <div>
+      <noscript>
+        <style>{`.nojs-show {opacity: 1, top: 0}`}</style>
+      </noscript>
+      <div className={styles.signedInStatus}>
+        <div
+          className={`nojs-show ${
+            !session && loading ? styles.loading : styles.loaded
+          }`}
+        >
           {!session && (
             <div>
               <span>You are not signed in</span>
               <br />
               <a href={`/api/auth/signin`} onClick={handleLogin}>
                 Sign in
+              </a>
+            </div>
+          )}
+
+          {session?.user && (
+            <div>
+              {session.user.image && (
+                <span
+                  style={{ backgroundImage: `url(${session.user.image})` }}
+                  className={styles.avatar}
+                />
+              )}
+              <span className={styles.signedInText}>
+                <small>Signed in as</small>
+                <br />
+                <strong>{session.user.email || session.user.name}</strong>
+              </span>
+              <a
+                href={`/api/auth/signout`}
+                className={styles.button}
+                onClick={handleLogout}
+              >
+                Sign out{' '}
               </a>
             </div>
           )}
